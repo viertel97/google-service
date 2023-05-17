46b13669f3d046a4438ad45b3a550c06abecddd1
Janik Viertel <janik@next-data-service.com>
2023-05-17 11:12:14 +0200

    added healthcheck

--- main.py
+++ main.py
@@ -23,6 +23,24 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
     return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 
+@app.get('/', status_code=status.HTTP_200_OK)
+def perform_healthcheck():
+    '''
+    Simple route for the GitHub Actions to healthcheck on.
+    More info is available at:
+    https://github.com/akhileshns/heroku-deploy#health-check
+    It basically sends a GET request to the route & hopes to get a "200"
+    response code. Failing to return a 200 response code just enables
+    the GitHub Actions to rollback to the last version the project was
+    found in a "working condition". It acts as a last line of defense in
+    case something goes south.
+    Additionally, it also returns a JSON response in the form of:
+    {
+      'healtcheck': 'Everything OK!'
+    }
+    '''
+    return {'healthcheck': 'Everything OK!'}
+
 if __name__ == "__main__":
     if platform.system() == "Windows":
         uvicorn.run(f"{Path(__file__).stem}:app", host="0.0.0.0", reload=True, port=8100)
